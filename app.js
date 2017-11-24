cd4da819dd65268fe4f6eb29fce523dda4d0ce56
Denis <denguk@gmail.com>
2017-11-24 15:44:05 +0500

    feat: update last success build

--- app.js
+++ app.js
@@ -67,6 +67,13 @@ async function getTags() {
       values.join(', '));
   }
 
+  console.log('Updating last success build...');
+  if (values.length > 0) {
+    const lastBuild = conn.query('SELECT id, ver FROM task WHERE template_id = ? ORDER BY id DESC LIMIT 1', config.templateID)[0];
+    await conn.query('UPDATE project__template SET last_success_build_task_id=?, last_success_version=? WHERE id = ?',
+      lastBuild.id, lastBuild.ver, config.templateID);
+  }
+
   console.log('Closing DB connection...');
   conn.end();
 })().catch(err => {
